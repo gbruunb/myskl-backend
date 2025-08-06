00da868498776e25e8e14a40f6697a110e99ba2d
GBru <unnameboy2546@hotmail.com>
2025-08-06 10:19:16 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/test', (c) => {
+  return c.text('Hello Test!')
+})
+
+serve({
+  fetch: app.fetch,
+  port: 3000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
